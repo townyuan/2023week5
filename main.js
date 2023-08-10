9665e2829453ab1a27b18025897b068769d85032
Mei Fang Chou <meifang@ettoday.net>
2023-08-10 17:50:35 +0800

    update content

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 import './assets/scss/all.scss';
 
-console.log("Hello world!");
+// console.log("Hello world!");
+
+// CKEditor
+ClassicEditor
+  .create( document.querySelector( '#editor' ) )
+  .then( editor => {
+  console.log( editor );
+} )
+  .catch( error => {
+  console.error( error );
+} );
+
+
+$(".btn-reply").click(function(){
+  $(".ck-editor").toggle();
+});
